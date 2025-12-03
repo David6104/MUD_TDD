d2ae4b861aa1fcbf3d358dacef54367881eb6fda
David <David.GASPAR>
2025-12-03 16:34:51 +0100

    question 7-8

---
+++ Box.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Box {
+    ArrayList<String> contents = new ArrayList<>();
+
+    Box(){
+        System.out.println("Box créée !");
+    }
+
+    public void add (String truc){
+        contents.add(truc);
+    }
+}
